a016340038a10e5173d7e16323ce16d8ccd5d293
Phuoc Do <dnprock@gmail.com>
2015-02-27 00:08:39 -0800

    Vertical sankey

--- document.js
+++ document.js
@@ -1,16 +1,90 @@
-/*--- GUIDELINES --- 
+/*--- IMPORTANT GUIDELINES --- 
 1. Use div #canvas-svg for svg rendering
     var svg = d3.select("#canvas-svg");
-2. 'data' variable contains JSON data
-   'config' variable contains data from Properties tab
-    Do NOT overwrite these variables
+2. 'data' variable contains JSON data from Data tab
+    Do NOT overwrite this variable 
 3. To define customizable properties, use capitalized variable names,
     and define them in Properties tab ---*/
 
-var WIDTH = config.width, HEIGHT = config.height;
+var margin = {top: 1, right: 20, bottom: 6, left: 20},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#canvas-svg")
-          .append("svg");
+var formatNumber = d3.format(",.0f"),
+    format = function(d) { return formatNumber(d) + " TWh"; },
+    color = d3.scale.category20();
+
+var svg = d3.select("#canvas-svg").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", width + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ") rotate(0)");
+
+var sankey = d3.sankey()
+    .nodeWidth(15)
+    .nodePadding(10)
+    .size([width, height]);
+
+var path = sankey.link();
+
+var energy = data;
+
+sankey
+    .nodes(energy.nodes)
+    .links(energy.links)
+    .layout(32);
+
+var link = svg.append("g")
+    .attr("class", "link-group")
+    .attr("transform", "translate(" + height + ", " + 0 + ") rotate(90)")
+    .selectAll(".link")
+    .data(energy.links)
+  .enter().append("path")
+    .attr("class", "link")
+    .attr("d", path)
+    .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+    .sort(function(a, b) { return b.dy - a.dy; });
+
+link.append("title")
+    .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+var node = svg
+    .append("g")
+    .attr("transform", "translate(" + height + ", " + 0 + ") rotate(90)")
+    .selectAll(".node")
+    .data(energy.nodes)
+  .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function(d) { return "translate(" + (d.x) + "," + (d.y) + ")"; })
+  .call(d3.behavior.drag()
+    .origin(function(d) { return d; })
+    .on("dragstart", function() { this.parentNode.appendChild(this); })
+    .on("drag", dragmove));
+
+node.append("rect")
+    .attr("height", function(d) { return d.dy; })
+    .attr("width", sankey.nodeWidth())
+    .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+    .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+  .append("title")
+    .text(function(d) { return d.name + "\n" + format(d.value); });
+
+node.append("text")
+    .attr("x", -6)
+    .attr("y", function(d) { return d.dx / 2; })
+    .attr("dy", ".35em")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(0)")
+    .text(function(d) { return d.name; })
+  .filter(function(d) { return d.x < width / 2; })
+    .attr("x", 6 + sankey.nodeWidth())
+    .attr("text-anchor", "start");
+
+function dragmove(d) {
+  d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+  sankey.relayout();
+  link.attr("d", path);
+}
 
 // Use sourceURL to enable debugging in Chrome
 //# sourceURL=document.js
